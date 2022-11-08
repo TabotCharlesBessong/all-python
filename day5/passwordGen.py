7cbe594f6154f315ab098b62fa1f5858b6e68f89
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-08 07:16:21 +0100

    finishing day 5 with password generator

--- day5/passwordGen.py
+++ day5/passwordGen.py
@@ -13,34 +13,74 @@ numLen = int(input("Number of numbers do you want your password to have : "))
 symbolLen = int(input("Number of symbols do you want your password to have? :"))
 
 # my own implementation (The easiest way to)
-password = []
-lettersPas = []
-numberPas = []
-symbolPas = []
-for i in range(0,letterNum):
-  ranL = letters[random.randint(0,letterNum)]
-  # lettersPas.append(letters[i])
-  lettersPas.append(ranL)
-print(lettersPas)  
+# password = []
+# lettersPas = []
+# numberPas = []
+# symbolPas = []
+# for i in range(0,letterNum):
+#   ranL = letters[random.randint(0,letterNum)]
+#   # lettersPas.append(letters[i])
+#   lettersPas.append(ranL)
+# print(lettersPas)  
   
-for i in range(0,numLen):
-  ranN = numbers[random.randint(0,numLen)]
-  # numberPas.append(numbers[i]) 
-  numberPas.append(ranN) 
-print(numberPas)  
+# for i in range(0,numLen):
+#   ranN = numbers[random.randint(0,numLen)]
+#   # numberPas.append(numbers[i]) 
+#   numberPas.append(ranN) 
+# print(numberPas)  
   
-for i in range(0,symbolLen):
-  ranS = symbols[random.randint(0,symbolLen)]
-  # symbolPas.append(symbols[i]) 
-  symbolPas.append(ranS) 
-print(symbolPas) 
+# for i in range(0,symbolLen):
+#   ranS = symbols[random.randint(0,symbolLen)]
+#   # symbolPas.append(symbols[i]) 
+#   symbolPas.append(ranS) 
+# print(symbolPas) 
 
-password = lettersPas + numberPas + symbolPas
-print(password) 
+# password = lettersPas + numberPas + symbolPas
+# print(password) 
 
-# tutors implementation
+# tutors implementation easy level
 
-password1 = ""
+# password1 = ""
+
+# for char in range(1, letterNum + 1):
+#   randomChar = random.choice(letters)
+#   password1 += randomChar
+# print(password1)
+  
+# for char in range(1, numLen + 1):
+#   randomChar = random.choice(numbers)
+#   password1 += randomChar
+# print(password1) 
+
+# for char in range(1, symbolLen + 1):
+#   randomChar = random.choice(symbols)
+#   password1 += randomChar
+# print(password1) 
+
+# tutors implementation hard level
+
+password2 = []
 
 for char in range(1, letterNum + 1):
-  randomChar = rn
+  randomChar = random.choice(letters)
+  password2 += randomChar
+# print(password2)
+  
+for char in range(1, numLen + 1):
+  randomChar = random.choice(numbers)
+  password2 += randomChar
+# print(password2) 
+
+for char in range(1, symbolLen + 1):
+  randomChar = random.choice(symbols)
+  password2 += randomChar
+# print(password2) 
+
+random.shuffle(password2)
+# print(password2)
+
+password3 = ""
+for char in password2 :
+  password3 += char
+
+print(f"Your password is : {password3} ")  
