0dab2286012f717e397668f6f4b35eeb5ddc2adb
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-09 13:46:14 +0100

    return

---
+++ day10/return.py
@@ -0,0 +1,27 @@
+
+def isLeap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+    
+
+def daysInMonth(year,month):
+  monthDays = [31,28,31,30,31,30,31,31,30,31,30,31] 
+  if isLeap(year):
+    monthDays[1] = 29
+  # index = monthDays.index(month)   
+  index = month - 1
+  
+  days = monthDays[index]  
+  return days
+year = int(input("Enter a year: ")) 
+month = int(input("Enter a month: ")) 
+days = daysInMonth(year,month)          
+print(f'There are {days} in this month ')        
