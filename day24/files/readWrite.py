c7c3085fa144c08088c21b4b9e8035d4f6bdb6e2
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-17 18:26:26 +0100

    read write into file

---
+++ day24/files/readWrite.py
@@ -0,0 +1,10 @@
+
+# file = open(r"day24\files\file.txt")
+# content = file.read()
+# print(content)
+# file.close()
+
+with open(r"day24\files\me.txt",mode="w") as file:
+  file.write("\nHello my name is Charles")
+  # content = file.read()
+  # print(content)
