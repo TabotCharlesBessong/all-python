85e5a8d23fc5d11c99377dd350c48e9aaaa7994b
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-17 09:52:03 +0100

    cross road game

---
+++ day23/player.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+  def __init__(self):
+    super().__init__()
+    self.shape("turtle")
+    self.penup()
+    self.goToStart()
+    self.setheading(90)
+
+  def goUp(self):
+      self.forward(MOVE_DISTANCE)
+
+  def goToStart(self):
+      self.goto(STARTING_POSITION)
+
+  def isAtFinishLine(self):
+      if self.ycor() > FINISH_LINE_Y:
+          return True
+      else:
+          return False
