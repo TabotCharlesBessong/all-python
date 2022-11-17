082fa2a007937b497e779e797134f99b8f4d20f2
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-11-17 06:45:32 +0100

    collision between paddle and ball (bounce)

--- day22/pong.py
+++ day22/pong.py
@@ -15,10 +15,10 @@ paddle2 = Paddle((280,0))
 ball = Ball()
 
 screen.listen()
-screen.onkey(paddle1.moveUp,"Up")
-screen.onkey(paddle1.moveDown,"Down")
-screen.onkey(paddle2.moveUp,"w")
-screen.onkey(paddle2.moveDown,"s")
+screen.onkey(paddle2.moveUp,"Up")
+screen.onkey(paddle2.moveDown,"Down")
+screen.onkey(paddle1.moveUp,"w")
+screen.onkey(paddle1.moveDown,"s")
 
 
 gameIsOn = True 
@@ -27,5 +27,16 @@ while gameIsOn:
   time.sleep(0.2)
   screen.update()
   ball.move()
+  
+  # detecting collision with the wall and the ball 
+  if ball.ycor() > 280 or ball.ycor() < -280:
+    # needs to bounce the ball
+    ball.bounceY()
+  # elif ball.xcor() > 280 or ball.xcor() < -280:
+  #   ball.gameOver() 
+    
+  # detecting collision between ball and paddle 
+  if ball.distance(paddle2) < 50 and ball.xcor() > 255 or ball.distance(paddle1) < 50 and ball.xcor() < -255 :
+    ball.bounceX()  
 
 screen.exitonclick() 
