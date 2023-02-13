5287d2d229e799de68e3e782d9557bbfc02de676
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2023-02-13 11:39:51 +0100

    understanding pattern 2

--- functions/patterns/pattern2.py
+++ functions/patterns/pattern2.py
@@ -12,14 +12,14 @@
 #            *
 
 def pattern(n):
-  k = (2 * n) - 2
+  k = int(((2 * n) - 2 ) * 1)
   for i in range(n,-1,-1): # to print n + 1 lines
-    for j in range(k,0,-1):
+    for j in range(k,0,-1): # for the spaces
       print(end=" ")
     k = k +1
-    for j in range(0,i+1):
+    for j in range(0,i+1): # after the spaces, you need to print the 
       print("*",end=" ")
-    print("\r")
+    print("\r") # move to the right
 
 num = int(input("Enter a number for a pattern: "))
 pattern(num)
