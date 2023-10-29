66418ad029aa1bf8c4adbda3f09ce724945d913a
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-29 10:40:35 +0100

    formating code for it to run properly

--- functions/patterns/pattern3.py
+++ functions/patterns/pattern3.py
@@ -1,29 +1,30 @@
 
-  #                *
-  #               * *
-  #              * * *
-  #             * * * *
-  #            * * * * *
-  #           * * * * * *
-  #          * * * * * * *
-  #         * * * * * * * *
-  #        * * * * * * * * *
-  #       * * * * * * * * * *
-  #      * * * * * * * * * * *
-  #     * * * * * * * * * * * *
-  #    * * * * * * * * * * * * *
-  #   * * * * * * * * * * * * * *
-  #  * * * * * * * * * * * * * * *
+#                *
+#               * *
+#              * * *
+#             * * * *
+#            * * * * *
+#           * * * * * *
+#          * * * * * * *
+#         * * * * * * * *
+#        * * * * * * * * *
+#       * * * * * * * * * *
+#      * * * * * * * * * * *
+#     * * * * * * * * * * * *
+#    * * * * * * * * * * * * *
+#   * * * * * * * * * * * * * *
+#  * * * * * * * * * * * * * * *
 
 def pattern(n):
-  k = int(((2 * n) - 2 ) * 1)
-  for i in range(0,n): # to print n lines
-    for j in range(0,k): # for the spaces
-      print(end=" ")
-    k = k - 1
-    for j in range(0,i+1): # after the spaces, you need to print the 
-      print("*",end=" ")
-    print("\r") # move to the right
+    k = int(((2 * n) - 2) * 1)
+    for i in range(0, n):  # to print n lines
+        for j in range(0, k):  # for the spaces
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i+1):  # after the spaces, you need to print the
+            print("*", end=" ")
+        print("\r")  # move to the right
+
 
 num = int(input("Enter a number for a pattern: "))
 pattern(num)
